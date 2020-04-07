2792725985c5d9edbe0aaf277d9792e146e09756
Mohamad Radisha <pr0ph0z23@gmail.com>
2020-04-07 20:45:16 +0700

    Update payload parser, make it less redundant

--- src/services/webhook_service.js
+++ src/services/webhook_service.js
@@ -1,22 +1,33 @@
 const parsePayload = (payload) => {
   const { action } = payload
-  const properties = {}
+  const type = payload.pull_request === undefined ? 'pull_request' : 'issue'
+  const properties = {
+    type: type,
+    url: payload['type'].html_url,
+    repository_name: payload.repository.name,
+    sender: {
+      username: payload.sender.login,
+      github_id: payload.sender.id
+    }
+  }
   switch (action) {
     case 'opened':
-      properties.type = 'issue'
-      properties.url = payload.issue.url
-      properties.sender.username = payload.sender.login
-      properties.sender.github_id = payload.sender.id
-      properties.assignees = payload.assignees.map(assignee => ({ username: asignee.login, github_id: asignee.id }))
+      properties.action = 'create'
+      properties.title = payload.issue.title
       break
     case 'assigned':
-      properties.type = 'issue'
-      properties.url = payload.issue.url
-      properties.sender.username = payload.sender.login
-      properties.sender.github_id = payload.sender.id
-      properties.assignees = payload.assignees.map(assignee => ({ username: asignee.login, github_id: asignee.id }))
+      properties.action = 'assign_issue'
+      properties.assignee.username = payload.assignee.login
+      properties.assignee.github_id = payload.assignee.id
+      break
+    case 'review_requested':
+      properties.action = 'review_pull'
+      properties.reviewer.username = payload.requested_reviewer.login
+      properties.reviewer.github_id = payload.requested_reviewer.id
       break
   }
+
+  return properties
 }
 
 module.exports = {
