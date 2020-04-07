50c61effec103a5bb626ce73aec0c6c118ef4908
Mohamad Radisha <pr0ph0z23@gmail.com>
2020-04-07 23:10:50 +0700

    Add object initialization, update way to accessing title, update ternary returned value

--- src/services/webhook_service.js
+++ src/services/webhook_service.js
@@ -1,11 +1,11 @@
 const parsePayload = (payload) => {
   const { action } = payload
-  const type = payload.pull_request === undefined ? 'pull_request' : 'issue'
+  const type = payload.pull_request === undefined ? 'issue' : 'pull_request'
   const properties = {
     type: type,
     url: payload[type].html_url,
     repository_name: payload.repository.name,
-    title: payload.issue.title,
+    title: payload[type].title,
     sender: {
       username: payload.sender.login,
       github_id: payload.sender.id
@@ -17,11 +17,13 @@ const parsePayload = (payload) => {
       break
     case 'assigned':
       properties.action = 'assign_issue'
+      properties.assignee = {}
       properties.assignee.username = payload.assignee.login
       properties.assignee.github_id = payload.assignee.id
       break
     case 'review_requested':
       properties.action = 'review_pull'
+      properties.reviewer = {}
       properties.reviewer.username = payload.requested_reviewer.login
       properties.reviewer.github_id = payload.requested_reviewer.id
       break
