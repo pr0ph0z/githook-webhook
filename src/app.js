7f31f26eed948fbca16f48bc8f756973bb0a2454
Mohamad Radisha <pr0ph0z23@gmail.com>
2020-04-08 19:03:00 +0700

    Add method to connect to AMQP, add a route

--- src/app.js
+++ src/app.js
@@ -3,11 +3,19 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const cors = require('cors')
+const amqp = require('./message_brokers/amqp')
+const logger = require('./utils/logger')
+
+amqp.connectToAmqp().then(_ => logger.info('AMQP connected!'))
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(morgan('dev'))
+
+const webhookController = require('./controllers/webhook_controller')
+
+app.post('/', webhookController.processWebhook)
 
 module.exports = app
